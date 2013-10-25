81ffedd4a085f04e577cc7f846d70d322c833bd5
edEssential <ed@essential-music.com>
2013-10-25 16:30:32 +0100

    mailer config

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -9,7 +9,7 @@ class ContactMailer < ActionMailer::Base
   
   def initiate_admin(contact)
     @contact = contact
-    mail(from: @contact.email, to: "ed_hamilton@hotmail.com", bcc: "ed_hamilton@live.com", subject: "New message from the website!")
+    mail(from: @contact.email, to: "lldk120@gmail.com", bcc: "ed_hamilton@live.com", subject: "New message from the website!")
   end
   
 end
