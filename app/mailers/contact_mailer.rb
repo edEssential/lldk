daedd5dd2a2c245887318e8a93dcda37b0f0695e
edEssential <ed@essential-music.com>
2013-10-25 15:53:09 +0100

    message now included in web contact

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,14 +2,14 @@ class ContactMailer < ActionMailer::Base
   
   default from: "\"info@dominichking.com\"<lldk120@gmail.com>"
   
-  def initiate_user(user)
-    @user = user
-    mail(to: @user.email, subject: "Thankyou for signing up!")
+  def initiate_user(contact)
+    @contact = user
+    mail(to: @contact.email, subject: "Thankyou for signing up!")
   end
   
-  def initiate_admin(user)
-    @user = user
-    mail(from: @user.email, to: "ed_hamilton@hotmail.com", bcc: "ed_hamilton@live.com", subject: "New message from the website!")
+  def initiate_admin(contact)
+    @contact = user
+    mail(from: @contact.email, to: "ed_hamilton@hotmail.com", bcc: "ed_hamilton@live.com", subject: "New message from the website!")
   end
   
 end
