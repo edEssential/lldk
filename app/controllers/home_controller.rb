440a1173c38df5dedd4e9309ad1312c6848b9c8b
edEssential <ed@essential-music.com>
2013-10-28 11:34:33 +0000

    homepage images now sliding

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
     @book_redux = Book.select('packshot_file_name, packshot_updated_at, review1, review2, review3, id').order('id DESC')
     @feed = Writing.first
     @tweets = Twitter.user_timeline("DominicHKing", :count => 4)
+    @books = Book.order('id DESC').all
   end
   
   def bring_in_book
