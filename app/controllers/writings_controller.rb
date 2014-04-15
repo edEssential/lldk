124a49550d8b924fc5ff247d19ef94e2df72141c
edEssential <ed@essential-music.com>
2014-04-15 17:30:29 +0100

    writings now order by id DESC

--- app/controllers/writings_controller.rb
+++ app/controllers/writings_controller.rb
@@ -1,7 +1,7 @@
 class WritingsController < ApplicationController
   
   def index
-    @feeds = Writing.paginate(:page => params[:page], :per_page => 1).order("id ASC")
+    @feeds = Writing.paginate(:page => params[:page], :per_page => 1).order("id DESC")
   end
 
 end
