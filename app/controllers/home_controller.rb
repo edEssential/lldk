8746a347d66a02916d847f210da8c12ca13d9eec
edEssential <ed@essential-music.com>
2014-04-16 10:49:42 +0100

    home page blog feed now pulling last entry

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   def index
     bring_in_book
     @book_redux = Book.select('packshot_file_name, packshot_updated_at, review1, review2, review3, id').order('id DESC')
-    @feed = Writing.first
+    @feed = Writing.last
     @tweets = Twitter.user_timeline("DominicHKing", :count => 4)
     @books = Book.order('id ASC').all
   end
