07c383a2275f382762c58bca5d5f575c2888b9f1
madebyeggs <madebyeggs@gmail.com>
2015-06-17 13:54:53 +0100

    assets cleaned

--- app/models/about.rb
+++ app/models/about.rb
@@ -1,6 +1,8 @@
 class About < ActiveRecord::Base
   attr_accessible :getintouchtext, :text, :avatar, :facebook, :twitter, :linkedin
-  has_attached_file :avatar, styles: {
+  has_attached_file :avatar, 
+                    :s3_protocol => 'https',
+                    styles: {
       medium: '274x300>'
     }
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,9 @@ class Book < ActiveRecord::Base
   attr_accessible :buylink1, :buylink2, :buylink3, :synopsis, :cover, :link1title, :link2title, :link3title, :title
 
     # This method associates the attribute ":avatar" with a file attachment
-    has_attached_file :cover, styles: {
+    has_attached_file :cover, 
+                      :s3_protocol => 'https',
+                      styles: {
       rectangle: '915x194>'
     }
 
